acfde2f69d0faef0aa93ff49875b566f254d608a
burgokmen <burgokmen@gmail.com>
2023-07-29 13:56:02 +0300

    Header Done, Project Ongoing

--- src/bileşenler/header.js
+++ src/bileşenler/header.js
@@ -11,7 +11,24 @@ const Header = (baslik, tarih, yazi) => {
   //    <span class="temp">{ yazi }</span>
   //  </div>
   //
-}
+  const div1 = document.createElement("div");
+  div1.setAttribute("class", "header");
+
+  const span1 = document.createElement("span");
+  span1.setAttribute("class", "date");
+  span1.textContent = tarih;
+
+  const h1a = document.createElement("h1");
+  h1a.textContent = baslik;
+
+  const span2 = document.createElement("span");
+  span2.setAttribute("class", "temp");
+  span2.textContent = yazi;
+
+  div1.append(span1, h1a, span2);
+
+  return div1;
+};
 
 const headerEkleyici = (secici) => {
   // GÖREV 2
@@ -20,10 +37,15 @@ const headerEkleyici = (secici) => {
   // Görev 1'de tanımladığınız Header bileşenini kullanarak bir header oluşturun (baslik,tarih,yazi parametrelerini kendi isteğinize göre belirleyin)
   // Oluşturulan header'i, verilen seçiciyle eşleşen DOM'daki öğeye eklemelidir.
   //
-
-  // İPUCU: querySelector bir string alabilir (bknz: querySelector("#wrapper")) 
+  // İPUCU: querySelector bir string alabilir (bknz: querySelector("#wrapper"))
   // fakat aynı zamanda bir değişken de alabilir (bknz: querySelector(secici))
- 
-}
+  const header1 = Header(
+    "BruCorp 1b usd degerlemeye ulasti!",
+    "08.01.2027",
+    "Buralar"
+  );
 
-export { Header, headerEkleyici }
+  document.querySelector(secici).append(header1);
+};
+
+export { Header, headerEkleyici };
